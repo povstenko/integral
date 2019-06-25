b5cbdd4eed68e11d7917bd18af86d24ede2acc3d
Vitaly Povstenko <vitaly.povstenko@gmail.com>
2019-06-25 11:40:38 +0300

    Update integral_db.sql

--- sql/integral_db.sql
+++ sql/integral_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Июн 22 2019 г., 11:18
+-- Время создания: Июн 25 2019 г., 10:16
 -- Версия сервера: 10.3.15-MariaDB
 -- Версия PHP: 7.3.6
 
@@ -88,19 +88,20 @@ CREATE TABLE `tests` (
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
-  `author` int(255) NOT NULL
+  `author` int(255) NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `tests`
 --
 
-INSERT INTO `tests` (`id`, `name`, `description`, `type`, `author`) VALUES
-(1, 'Maths Test 1', 'Maths test 1 description', 'maths', 2),
-(3, 'Maths test 2', 'Maths test 2 description', 'maths', 2),
-(4, 'Physics test 1', 'Physics test 1 description', 'physics', 2),
-(5, 'Maths test 3', 'Maths test 3 description', 'maths', 2),
-(6, 'Physics test 2', 'Physics test 2 description', 'physics', 2);
+INSERT INTO `tests` (`id`, `name`, `description`, `type`, `author`, `date`) VALUES
+(1, 'Maths Test 1', 'Maths test 1 description', 'maths', 2, '0000-00-00'),
+(3, 'Maths test 2', 'Maths test 2 description', 'maths', 2, '0000-00-00'),
+(4, 'Physics test 1', 'Physics test 1 description', 'physics', 2, '0000-00-00'),
+(5, 'Maths test 3', 'Maths test 3 description', 'maths', 2, '0000-00-00'),
+(6, 'Physics test 2', 'Physics test 2 description', 'physics', 2, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -112,16 +113,17 @@ CREATE TABLE `users` (
   `id` int(255) UNSIGNED NOT NULL,
   `login` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_token` varchar(255) CHARACTER SET utf8 DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `email`, `password`) VALUES
-(2, 'admin', 'admin@admin', '$2y$10$nP60oUv7mIS/.hQObgLKJO77/7cGJUtCtr2OnyY9KlgGkTXVd9ntW'),
-(3, 'tester', 'tester@tester', '$2y$10$TbEO2OxRNA363z1KwMWRROkokQt1OX/rYXOSLQoE7uf/lZz0mxICe');
+INSERT INTO `users` (`id`, `login`, `email`, `password`, `user_token`) VALUES
+(2, 'admin', 'admin@admin', '$2y$10$nP60oUv7mIS/.hQObgLKJO77/7cGJUtCtr2OnyY9KlgGkTXVd9ntW', 'viJr0bFsSvo22osANcTZEKt8aw7r2B6f4zDDbxsD4I7AF34MJnAEasUYJTvxtd4DMQAfnj9kZxYREUdI'),
+(3, 'tester', 'tester@tester', '$2y$10$TbEO2OxRNA363z1KwMWRROkokQt1OX/rYXOSLQoE7uf/lZz0mxICe', '');
 
 --
 -- Индексы сохранённых таблиц
